6d8e8741c10fe46ee2bb251fc30a60971b24e121
thechib12 <christiaanvdbogaard@gmail.com>
2016-06-15 13:38:50 +0200

    TESTSSSSSSSSSSSSSSSSSS #dhekp

---
+++ src/tests/java/testutils/ParseException.java
@@ -0,0 +1,31 @@
+package testutils;
+
+import java.util.List;
+
+/**
+ * Exception class wrapping a list of error messages.
+ */
+public class ParseException extends Exception {
+    private final List<String> messages;
+
+    public ParseException(List<String> messages) {
+        super(messages.toString());
+        this.messages = messages;
+    }
+
+    /**
+     * Returns the error messages wrapped in this exception.
+     */
+    public List<String> getMessages() {
+        return this.messages;
+    }
+
+    /**
+     * Prints all error messages to stdout, line by line.
+     */
+    public void print() {
+        for (String error : getMessages()) {
+            System.out.println(error);
+        }
+    }
+}
