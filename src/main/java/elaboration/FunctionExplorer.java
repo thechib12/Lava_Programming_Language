f3886f773af95fbfd924967f4cc7c89858d2b3d8
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-06-22 11:24:47 +0200

    function explorer stub

---
+++ src/main/java/elaboration/FunctionExplorer.java
@@ -0,0 +1,17 @@
+package elaboration;
+
+import grammar.LavaBaseListener;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.Map;
+
+/**
+ * Created by Rogier on 22-06-16 in Enschede.
+ */
+public class FunctionExplorer extends LavaBaseListener {
+
+
+    public Map<String, Type> explore(ParseTree tree) {
+        return null;
+    }
+}
